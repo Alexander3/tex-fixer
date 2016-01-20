d7ebfcbac0ef5668ecf432341021104ef4c0a40b
Aleksander <kawalaaleksander@gmail.com>
2016-01-20 15:59:32 +0100

    Big refactor, added help and argparsing

--- corect endings.py
+++ corect endings.py
@@ -1,26 +1,47 @@
-import os
+from os import path
+from glob import glob
+import argparse
 
-
-name_of_main_file = "inż"
-path_to_tex_files = "../tex"
-
-list_of_words = ["a", "i", "o", "u", "w", "z", "ze", "od", "do", "że", "iż",
+LIST_OF_WORDS = ["a", "i", "o", "u", "w", "z", "ze", "od", "do", "że", "iż",
                  "poprzez", "spod", "sponad", "znad", "po", "za", "nad", "pod", "na",
                  "oraz", "ale", "lub", "albo", "bądź", "czy", "by", "aby", "jak", "ponieważ", "bo",
                  "który", "która", "które", "którego", "krótemu", "której", "którym"]
-uppercased = [word.title() for word in list_of_words]
-list_of_words += uppercased
 
-for file in filter(lambda x: x.endswith(".tex"), os.listdir(path_to_tex_files)):
-    if not file.startswith(name_of_main_file):
+
+def add_hard_spaces(path_to_tex_files):
+    global LIST_OF_WORDS
+    uppercased = [word.title() for word in LIST_OF_WORDS]
+    LIST_OF_WORDS += uppercased
+
+    files = glob(path.join(path_to_tex_files, '*.tex'))
+
+    for file in files:
         print("fixing: " + file)
-        with open(os.path.join(path_to_tex_files, file), encoding='utf-8') as f:
+        with open(file, encoding='utf-8') as f:
             lines = f.readlines()
-        with open(os.path.join(path_to_tex_files, file), encoding='utf-8', mode="w") as out:
+        with open(file, encoding='utf-8', mode="w") as out:
             for line in lines:
-                if not (line.startswith("\\") or line.startswith("%")):
-                    for word in list_of_words:
-                        line = line.replace(" " + word + " ", " " + word + "~")
-                        if line.startswith(word + " "):
-                            line = line.replace(word + " ", word + "~", 1)
-                out.write(line)
+                out.write(fix_line(line))
+
+
+def fix_line(line):
+    global LIST_OF_WORDS
+    if not (line.startswith("\\") or line.startswith("%")):
+        for word in LIST_OF_WORDS:
+            line = line.replace(" " + word + " ", " " + word + "~")
+            if line.startswith(word + " "):
+                line = line.replace(word + " ", word + "~", 1)
+    return line
+
+
+def cofigure_args_and_help():
+    parser = argparse.ArgumentParser(description='This script adds hard spaces (~) to your .tex files.')
+    parser.add_argument('PATH', type=str, nargs='?', default='.',
+                        help='Path to directory with your .tex files')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = cofigure_args_and_help()
+    add_hard_spaces(args.PATH)
+
